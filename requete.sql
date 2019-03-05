0dcc70cf78d710e7766231132b14f379dd74bc44
rubenavone <navone_ruben@msn.com>
2019-03-05 17:07:29 +0100

    Ajout de la fin du tp 2

--- requete.sql
+++ requete.sql
@@ -1,4 +1,5 @@
 
+--TP 1
 INSERT INTO `lexique`(`id`, `mot`, `definition`, `type`) VALUES (NULL, 'For', 'La boucle for est en informatique une structure de contrôle de programmation permettant de réaliser une boucle associée à une variable entière ou un pointeur qui sera incrémentée à chaque itération. Il est souvent utilisé pour exploiter les données d''une collection indexée.', 'Globale'),
 (NULL, 'While', 'En programmation, la boucle while, francisée en boucle tant que, est une structure de contrôle permettant d''exécuter un ensemble d''instructions de façon répétée sur la base d''une condition booléenne. La boucle while peut être considérée comme une répétition de l''instruction if.', 'Globale'),
 (NULL, 'if else if else', 'Structure de contrôle En programmation informatique, une structure de contrôle est une instruction particulière d''un langage de programmation impératif pouvant dévier le flot de contrôle du programme la contenant lorsqu''elle est exécutée', 'Global'),
@@ -13,6 +14,7 @@ INSERT INTO `lexique`(`id`, `mot`, `definition`, `type`) VALUES (NULL, 'For', 'L
 */
 SELECT people.nom, prenom, Sport.nom FROM `pratique` , people , Sport WHERE people.id = pratique.id_nom AND Sport.id = pratique.id_sport 
 
+--TP 2
    -- Afficher tous les jeux sans afficher l'id
 
 SELECT `jeux_video`.`nom`, `jeux_video`.`possesseur`, `jeux_video`.`console`, `jeux_video`.`nbre_joueurs_max`, `jeux_video`.`prix`,`jeux_video`.`commentaires`
@@ -44,17 +46,19 @@ SELECT AVG( `jeux_video`.`nbre_joueurs_max` ) FROM `jeux_video` ;
 SELECT MAX(jeux_video.prix) FROM jeux_video --je vais i reflechir
 
    -- Afficher les jeux qui sont sur "Xbox"
-SELECT jeux_video.console FROM jeux_vide WHERE jeux_video.console = 'xbox'
+SELECT jeux_video.nom FROM jeux_video WHERE jeux_video.console = 'xbox'
 
    -- Affcher le nombre de jeux qui sont sur la "Xbox"
+SELECT COUNT(jeux_video.console) FROM jeux_video WHERE jeux_video.console = 'xbox'
 
    -- Afficher le nombre de jeux par console
-
+SELECT jeux_video.console , COUNT(nom) FROM jeux_video GROUP BY jeux_video.console
    -- Afficher le nombre de jeux par personne
-
+SELECT jeux_video.possesseur, Count(possesseur) FROM jeux_video GROUP BY jeux_video.possesseur
    -- Afficher les jeux qui contiennent la lettre "x" dans leur nom
-
+SELECT jeux_video.nom FROM jeux_video WHERE nom LIKE '%x%'
    -- Afficher les jeux qui commence par "fi"
+SELECT jeux_video.nom FROM jeux_video WHERE nom LIKE '%fi'
 
    -- Repondre à la question : "Quels sont les personnes qui possèdent les jeux qui commence par "fi" ?"
 
